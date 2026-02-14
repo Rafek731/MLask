53fe09ec9d2f682b8fbf2aeba53296a1bfe2ca9c
Rafek731 <raf.mazur.000@gmail.com>
2026-02-14 16:36:11 +0100

    New comments and functions to implement

--- src/Mat.h
+++ src/Mat.h
@@ -1,10 +1,7 @@
 #pragma once
-
-namespace mlask{
-    typedef float float_t;
-    typedef int int_t;
-
-    class Mat final{
+#include "types.h"
+namespace mlask {
+    class Mat final {
     private:
         int_t rows;
         int_t cols;
@@ -16,35 +13,50 @@ namespace mlask{
         Mat(const Mat& mat);
         ~Mat();
 
+        // Addidtion
         Mat operator+(const Mat& other);
         Mat& operator+=(const Mat& other);
         Mat operator+(const float_t scalar);
         Mat& operator+=(const float_t scalar);
         
+        // Substraction
         Mat operator-(const Mat& other);
         Mat& operator-=(const Mat& other);
         Mat operator-(const float_t scalar);
         Mat& operator-=(const float_t scalar);
 
+        // Multiplication
         Mat operator*(const Mat& other);
         Mat& operator*=(const Mat& other);
         Mat operator*(const float_t scalar);
         Mat& operator*=(const float_t scalar);
 
+        // Division
         Mat operator/(const float_t scalar);
         Mat& operator/=(const float_t scalar);
 
+        // Matching
         bool operator==(const Mat& other);
         bool operator!=(const Mat& other);
 
-        bool tryGet(int_t row, int_t col, float_t& value);
-        float_t& operator()(int_t row, int_t col);
+        
         const float_t& operator()(int_t row, int_t col) const;
+        float_t& operator()(int_t row, int_t col);
         float_t* operator[](int idx);
+        
+        bool tryGet(int_t row, int_t col, float_t& value);
 
         Mat copy();
-
         void print();
+
+        // TODO: implement
+        Mat transpose();
+        Mat inverse(); // Subclass for square matrix?
+        float_t det(); // Subclass for square matrix?
+        Mat adj();     // Subclass for square matrix?
+        Mat cross(int_t row, int_t col);
+        int rank();
+
     };
 
     void print_row(float_t *row, int_t row_len);
