d43e82b6aa41005b44715bda8ac7b6e65e0e6d54
Rafek731 <raf.mazur.000@gmail.com>
2026-04-21 19:25:47 +0200

    Added types for universal type usage accross whole project

---
+++ types.hpp
@@ -0,0 +1,14 @@
+#include <stdfloat>
+#include <cstddef>
+#include <cstdint>
+
+namespace mlask {
+    /**
+     * All types used in this project are coded here,
+     * you can always override them in any file
+     */
+    using float_t = std::float32_t;
+    using size_t = std::size_t;
+    using int_t = std::int32_t;
+    using uint_t = std::uint32_t;
+}
